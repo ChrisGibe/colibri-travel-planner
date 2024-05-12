e9b1c880238f79778f75d0e9954f7391322a6c48
Christophe Gibelli <cgibelli@tequilarapido.com>
2024-05-12 16:15:41 +0200

    Init Curtains

--- js/modules/lenis.js
+++ js/modules/lenis.js
@@ -1,16 +1,12 @@
-import Lenis from 'lenis'
+import Lenis from "lenis";
 
 export const initLenis = () => {
-    const lenis = new Lenis()
+  const lenis = new Lenis();
 
-    lenis.on('scroll', (e) => {
-    console.log(e)
-    })
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
 
-    function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-}
+  requestAnimationFrame(raf);
+};
