972cb2d3e45c9941ec556b002b983e5fb26589ee
Christophe Gibelli <cgibelli@tequilarapido.com>
2024-09-07 18:16:38 +0200

    Change root for load fonts inside app.js

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ import {loadFonts} from "./helpers/loadFonts";
 
 // Liste des polices à charger avec leurs poids et styles
 const fontsToLoad = [
-  {name: "unbounded", url: "./public/fonts/unbounded/Unbounded-Regular.woff2", weight: "400", style: "normal"},
-  {name: "unbounded", url: "./public/fonts/unbounded/Unbounded-Bold.woff2", weight: "700", style: "normal"},
-  {name: "poppins", url: "./public/fonts/poppins/Poppins-Regular.woff2", weight: "400", style: "normal"},
-  {name: "poppins", url: "./public/fonts/poppins/Poppins-Bold.woff2", weight: "700", style: "normal"},
+  {name: "unbounded", url: "/fonts/unbounded/Unbounded-Regular.woff2", weight: "400", style: "normal"},
+  {name: "unbounded", url: "/fonts/unbounded/Unbounded-Bold.woff2", weight: "700", style: "normal"},
+  {name: "poppins", url: "/fonts/poppins/Poppins-Regular.woff2", weight: "400", style: "normal"},
+  {name: "poppins", url: "/fonts/poppins/Poppins-Bold.woff2", weight: "700", style: "normal"},
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
